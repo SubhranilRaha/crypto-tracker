ef348f6f711ce6a0d336f553a0594ccf5ac73bfb
SubhranilRaha <82709110+SubhranilRaha@users.noreply.github.com>
2022-12-01 23:14:49 +0530

    pagination component added

--- src/Components/CoinsTable.jsx
+++ src/Components/CoinsTable.jsx
@@ -80,6 +80,11 @@ const CoinsTable = () => {
         </Link>
         );
       })}
+      <div class="btn-group">
+  <button class="btn">«</button>
+  <button class="btn">Page 22</button>
+  <button class="btn">»</button>
+</div>
     </div>
   )
 }
